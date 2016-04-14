b8b5a33d1d1ed9011fac19b08b8ba1c18aed14d7
sugieri44 at bc <sugieri44@bellevuecollege.edu>
2016-04-13 17:18:55 -0700

    Added Week02 folders

---
+++ Week02-ObjectBasicsJasmine/Source/MyObject.js
@@ -0,0 +1,24 @@
+
+var myObject = {
+
+    getOne: function() {
+        'use strict';
+        return 1;
+    },
+
+    getTwo: function() {
+        'use strict';
+        return 2;
+    },
+
+    getThree: function() {
+        'use strict';
+        return 3;
+    },
+
+    today: 04132016,
+    tomorrow: 04142016,
+    myName: "Erina"
+};
+
+module.exports = myObject;
