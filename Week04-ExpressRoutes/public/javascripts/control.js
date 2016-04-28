69ce5b7c5e38c29e5fd6f1dfc1a836abef941873
sugieri44 at bc <sugieri44@bellevuecollege.edu>
2016-04-27 17:34:36 -0700

    Modified Week04 project

--- Week04-ExpressRoutes/public/javascripts/control.js
+++ Week04-ExpressRoutes/public/javascripts/control.js
@@ -5,20 +5,29 @@ $(document).ready(function(){
     console.log('Docuemnt loaded');
     $('#read').click(callRead);
     $('#readJson').click(callReadJson);
+    $('#readQux').click(callQux);
 
     function callRead(){
         console.log('callRead called');
-        $.getJSON('/read', function(result){
+        $.getJSON('/read', function(result){ /**== $.getJSON('http://localhost:30025/read', function(result){**/
             console.log(result);
             $('#display').html(JSON.stringify(result)); //Convert to a JSON strings
         })
     }
 
     function callReadJson(){
-        console.log('callRead called');
+        console.log('callReadJson called'); //Displayed on a browser console
         $.getJSON('name.json', function(result){
             console.log(result);
             $('#display').html(JSON.stringify(result));
+        })
+    }
+
+    function callQux(){
+        console.log('callQux called');
+        $.getJSON('/qux', function(result){ /**== $.getJSON('http://localhost:30025/read', function(result){**/
+        console.log(result);
+            $('#display').html(JSON.stringify(result)); //Convert to a JSON strings
         })
     }
 
--- Week04-ExpressRoutes/routes/index.js
+++ Week04-ExpressRoutes/routes/index.js
@@ -7,10 +7,24 @@ router.get('/', function(req, res, next) {//parent page /
 });
 
 router.get('/read', function(request,response){//child-page /read
+  console.log('Read was called on the server', request.query); //print out the content of the query on the server's console
   response.send([
     {name: 'SarahLee'},
-    {name: 'Boo'}
-  ]);// -> When called, it's convered to JSON (double-quote around 'name')
+    {name: 'Boo', myArray: [1,2,3]},
+    {result: parseInt(request.query.operatorA) + parseInt(request.query.operatorB)}
+  ]);// -> When called, it's convered to JSON (double-quote around 'name'
+});
+
+router.get('/qux', function(request,response){//child-page /qux
+  console.log('Qux was called on the server');//Displayed on this server's console
+  response.send([
+    {name: 'QuxLee'},
+    {name: 'Qux'}
+  ]);
+});
+
+router.get('/:id', function(request,response){//glab whatever passed in
+  response.send(request.params.id);//and display
 });
 
 module.exports = router;
